7327ebc9b5b8528c9e5b1a29b5c1bdec673688b2
Eric Wilson <wilson.eric.n@gmail.com>
2012-11-08 22:35:45 -0500

    moved tests into separate ns

--- src/wilsonericn/mm.clj
+++ src/wilsonericn/mm.clj
@@ -1,5 +1,4 @@
-(ns wilsonericn.mm
-  (:use [clojure.test]))
+(ns wilsonericn.mm)
 
 (def pegs #{:red :orange :yellow :green :blue :violet})
 
@@ -16,31 +15,3 @@
   (let [exact (exact-matches guess actual)
         unordered (unordered-matches guess actual)]
     {:black exact :white (- unordered exact)}))
-
-(run-all-tests)
-
-(deftest finds-exact-matches 
-  (is (= 0 (exact-matches [:red :red :blue :green] [:yellow :blue :green :red])))
-  (is (= 1 (exact-matches [:red :red :blue :green] [:red :blue :green :red])))
-  (is (= 2 (exact-matches [:red :red :blue :green] [:red :blue :green :green])))
-  (is (= 3 (exact-matches [:red :red :blue :green] [:red :red :green :green])))
-  (is (= 4 (exact-matches [:red :red :blue :green] [:red :red :blue :green]))))
-
-(deftest finds-unordered-matches 
-  (is (= 0 (unordered-matches [:red :red :blue :green] [:yellow :violet :orange :orange])))
-  (is (= 1 (unordered-matches [:red :red :blue :green] [:yellow :blue :violet :yellow])))
-  (is (= 2 (unordered-matches [:red :red :blue :green] [:red :yellow :red :yellow])))
-  (is (= 3 (unordered-matches [:red :red :blue :green] [:red :blue :green :green])))
-  (is (= 4 (unordered-matches [:red :red :blue :green] [:green :red :red :blue]))))
-
-(deftest evaluates-guesses 
-  (is (= {:black 0 :white 0} (evaluate [:red :red :blue :green] [:yellow :violet :orange :orange])))
-  (is (= {:black 0 :white 1} (evaluate [:red :red :blue :green] [:yellow :blue :violet :yellow])))
-  (is (= {:black 1 :white 1} (evaluate [:red :red :blue :green] [:red :yellow :red :yellow])))
-  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
-  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:green :red :red :blue])))
-  (is (= {:black 0 :white 3} (evaluate [:red :red :blue :green] [:yellow :blue :green :red])))
-  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:red :blue :green :red])))
-  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
-  (is (= {:black 3 :white 0} (evaluate [:red :red :blue :green] [:red :red :green :green])))
-  (is (= {:black 4 :white 0} (evaluate [:red :red :blue :green] [:red :red :blue :green]))))
---
+++ test/wilsonericn/mm_test.clj
@@ -0,0 +1,33 @@
+(ns wilsonericn.mm-test
+  (:use clojure.test
+        wilsonericn.mm))
+
+(run-all-tests)
+
+(deftest finds-exact-matches 
+  (is (= 0 (exact-matches [:red :red :blue :green] [:yellow :blue :green :red])))
+  (is (= 1 (exact-matches [:red :red :blue :green] [:red :blue :green :red])))
+  (is (= 2 (exact-matches [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= 3 (exact-matches [:red :red :blue :green] [:red :red :green :green])))
+  (is (= 4 (exact-matches [:red :red :blue :green] [:red :red :blue :green]))))
+
+(deftest finds-unordered-matches 
+  (is (= 0 (unordered-matches [:red :red :blue :green] [:yellow :violet :orange :orange])))
+  (is (= 1 (unordered-matches [:red :red :blue :green] [:yellow :blue :violet :yellow])))
+  (is (= 2 (unordered-matches [:red :red :blue :green] [:red :yellow :red :yellow])))
+  (is (= 3 (unordered-matches [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= 4 (unordered-matches [:red :red :blue :green] [:green :red :red :blue]))))
+
+(deftest evaluates-guesses 
+  (is (= {:black 0 :white 0} (evaluate [:red :red :blue :green] [:yellow :violet :orange :orange])))
+  (is (= {:black 0 :white 1} (evaluate [:red :red :blue :green] [:yellow :blue :violet :yellow])))
+  (is (= {:black 1 :white 1} (evaluate [:red :red :blue :green] [:red :yellow :red :yellow])))
+  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:green :red :red :blue])))
+  (is (= {:black 0 :white 3} (evaluate [:red :red :blue :green] [:yellow :blue :green :red])))
+  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:red :blue :green :red])))
+  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= {:black 3 :white 0} (evaluate [:red :red :blue :green] [:red :red :green :green])))
+  (is (= {:black 4 :white 0} (evaluate [:red :red :blue :green] [:red :red :blue :green]))))
+
+
