b266626079fc660857fc9795dd53b80a9e4413f9
Eric Wilson <wilson.eric.n@gmail.com>
2012-11-08 21:39:22 -0500

    first commit

---
+++ src/wilsonericn/mm.clj
@@ -0,0 +1,29 @@
+(ns wilsonericn.mm
+  (:use [clojure.test]))
+
+(def pegs #{:red :orange :yellow :green :blue :violet })
+
+(defn exact-matches [guess actual]
+  (count (filter true? (map = guess actual))))
+
+(defn unordered-matches [guess actual]
+  (let [guess-cts (frequencies guess)
+        actual-cts (frequencies actual)
+        common-keys (filter (set (keys guess-cts)) (keys actual-cts))]
+    (apply + (map (fn [k] (Math/min (k guess-cts) (k actual-cts))) common-keys))))
+
+(run-all-tests)
+
+(deftest finds-exact-matches 
+  (is (= 0 (exact-matches [:red :red :blue :green] [:yellow :blue :green :red])))
+  (is (= 1 (exact-matches [:red :red :blue :green] [:red :blue :green :red])))
+  (is (= 2 (exact-matches [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= 3 (exact-matches [:red :red :blue :green] [:red :red :green :green])))
+  (is (= 4 (exact-matches [:red :red :blue :green] [:red :red :blue :green]))))
+
+(deftest finds-unordered-matches 
+  (is (= 0 (unordered-matches [:red :red :blue :green] [:yellow :violet :orange :orange])))
+  (is (= 1 (unordered-matches [:red :red :blue :green] [:yellow :blue :violet :yellow])))
+  (is (= 2 (unordered-matches [:red :red :blue :green] [:red :yellow :red :yellow])))
+  (is (= 3 (unordered-matches [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= 4 (unordered-matches [:red :red :blue :green] [:green :red :red :blue]))))
