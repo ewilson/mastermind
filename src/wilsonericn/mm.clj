cbf11b402fc338b38cdaff780da745496d518fb5
Eric Wilson <wilson.eric.n@gmail.com>
2012-11-08 22:08:56 -0500

    Added evaluate method to return counts of black & white pegs

--- src/wilsonericn/mm.clj
+++ src/wilsonericn/mm.clj
@@ -1,7 +1,7 @@
 (ns wilsonericn.mm
   (:use [clojure.test]))
 
-(def pegs #{:red :orange :yellow :green :blue :violet })
+(def pegs #{:red :orange :yellow :green :blue :violet})
 
 (defn exact-matches [guess actual]
   (count (filter true? (map = guess actual))))
@@ -11,6 +11,11 @@
         actual-cts (frequencies actual)
         common-keys (filter (set (keys guess-cts)) (keys actual-cts))]
     (apply + (map (fn [k] (Math/min (k guess-cts) (k actual-cts))) common-keys))))
+
+(defn evaluate [guess actual] 
+  (let [exact (exact-matches guess actual)
+        unordered (unordered-matches guess actual)]
+    {:black exact :white (- unordered exact)}))
 
 (run-all-tests)
 
@@ -27,3 +32,15 @@
   (is (= 2 (unordered-matches [:red :red :blue :green] [:red :yellow :red :yellow])))
   (is (= 3 (unordered-matches [:red :red :blue :green] [:red :blue :green :green])))
   (is (= 4 (unordered-matches [:red :red :blue :green] [:green :red :red :blue]))))
+
+(deftest evaluates-guesses 
+  (is (= {:black 0 :white 0} (evaluate [:red :red :blue :green] [:yellow :violet :orange :orange])))
+  (is (= {:black 0 :white 1} (evaluate [:red :red :blue :green] [:yellow :blue :violet :yellow])))
+  (is (= {:black 1 :white 1} (evaluate [:red :red :blue :green] [:red :yellow :red :yellow])))
+  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:green :red :red :blue])))
+  (is (= {:black 0 :white 3} (evaluate [:red :red :blue :green] [:yellow :blue :green :red])))
+  (is (= {:black 1 :white 3} (evaluate [:red :red :blue :green] [:red :blue :green :red])))
+  (is (= {:black 2 :white 1} (evaluate [:red :red :blue :green] [:red :blue :green :green])))
+  (is (= {:black 3 :white 0} (evaluate [:red :red :blue :green] [:red :red :green :green])))
+  (is (= {:black 4 :white 0} (evaluate [:red :red :blue :green] [:red :red :blue :green]))))
