2d524f46d856be0b5ee025b4aad0534cd91d8659
Eric Wilson <wilson.eric.n@gmail.com>
2012-11-16 23:34:18 -0500

    allowed omitting of console output for multiple game play

--- src/wilsonericn/game.clj
+++ src/wilsonericn/game.clj
@@ -3,12 +3,12 @@
         wilsonericn.cli
         wilsonericn.solver))
 
-(defn play-game [input-secret input-guess]
+(defn play-game [input-secret input-guess output]
   (let [secret (input-secret)]
     (loop [count 1
            rounds []]
       (cond
-        (= (:clue (last rounds)) (repeat 4 :black)) (show-board rounds)
+        (= (:clue (last rounds)) (repeat 4 :black)) (output rounds)
         (> count 8) (losing-message rounds secret)
         :else (let [guess (input-guess rounds)
                     round {:guess guess :clue (evaluate guess secret) :round count}]
@@ -16,11 +16,11 @@
 
 (defn play [type]
   (cond 
-    (= type :encode) (play-game request-code request-comp-guess)
-    (= type :decode) (play-game choose-secret request-guess)
+    (= type :encode) (play-game request-code request-comp-guess show-board)
+    (= type :decode) (play-game choose-secret request-guess show-board)
     :default "ERROR"))
 
 (defn play-all [n]
   (let [secrets (take n (map (fn [code] (fn [] code)) allcodes))]
-    (for [secret secrets] (play-game secret request-comp-guess))))
+    (for [secret secrets] (play-game secret request-comp-guess identity))))
     
